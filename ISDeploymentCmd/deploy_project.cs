4726799b0a7716976c9a1708ee9ec8892f895db0
unclebiguns <corbett.jack@gmail.com>
2017-03-18 10:25:37 -0400

    Added CreateFolder function. Edited DeployProject to call CreateFolder

--- ISDeploymentCmd/deploy_project.cs
+++ ISDeploymentCmd/deploy_project.cs
@@ -8,8 +8,10 @@ namespace ISDeploymentCmd
     {
         public int DeployProject(string folderName, string projectName, byte[] projectStream)
         {
+
             using (var connection = new SqlConnection(ConnectionString))
             {
+                int folderCreated = CreateFolder(folderName, connection);
                 using (var cmd = new SqlCommand())
                 {
                     cmd.Connection = connection;
@@ -19,10 +21,59 @@ namespace ISDeploymentCmd
                     cmd.Parameters.Add("@project_name", SqlDbType.NVarChar).Value = projectName;
                     cmd.Parameters.Add("@project_stream", SqlDbType.VarBinary).Value = projectStream;
                     cmd.Parameters.Add("@operation_id", SqlDbType.BigInt).Direction = ParameterDirection.Output;
-                    connection.Open();
+                    if (connection.State!=ConnectionState.Open)
+                    {
+                        connection.Open();
+                    }
                     return Convert.ToInt32(cmd.ExecuteScalar());
                 }
             }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        public int CreateFolder(string folderName, SqlConnection connection = null)
+        {
+            if (connection.Equals(null))
+            {
+                connection = new SqlConnection(ConnectionString);
+            }
+            using (var cmd = new SqlCommand())
+            {
+                cmd.Connection = connection;
+                cmd.CommandText = "catalog.create_folder";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@folder_name", SqlDbType.NVarChar).Value = folderName;
+                cmd.Parameters.Add("@folder_id", SqlDbType.BigInt).Direction = ParameterDirection.Output;
+                connection.Open();
+                try
+                {
+                    int rc = Convert.ToInt32(cmd.ExecuteScalar());
+                    return rc;
+                }
+                catch (SqlException SqlEx)
+                {
+                    // if folder exists error don't error out
+                    if (SqlEx.Number== 27190)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+                
+            }
+           
+        }
     }
-}
+};
