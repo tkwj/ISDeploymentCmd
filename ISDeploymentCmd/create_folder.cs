d81363e62ef73bd64349ba347992e9c32be2de6a
Tom Willwerth <tom@tkwj.com>
2017-03-19 14:30:18 -0400

    refactored function into create_folder.cs

---
+++ ISDeploymentCmd/create_folder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ISDeploymentCmd
+{
+    public partial class CatalogProc
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        public int CreateFolder(string folderName, SqlConnection connection = null)
+        {
+            if (string.IsNullOrEmpty(folderName))
+            {
+                Exception ex = new Exception("FolderName must be supplied");
+                throw ex;
+            }
+
+            if (connection == null)
+            {
+                connection = new SqlConnection(ConnectionString);
+            }
+            using (var cmd = new SqlCommand())
+            {
+                cmd.Connection = connection;
+                cmd.CommandText = "catalog.create_folder";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@folder_name", SqlDbType.NVarChar).Value = folderName;
+                cmd.Parameters.Add("@folder_id", SqlDbType.BigInt).Direction = ParameterDirection.Output;
+                connection.Open();
+                try
+                {
+                    int rc = Convert.ToInt32(cmd.ExecuteScalar());
+                    return rc;
+                }
+                catch (SqlException sqlEx)
+                {
+                    // if folder exists error don't error out
+                    if (sqlEx.Number == 27190)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+           
+        }
+    }
+};
--- ISDeploymentCmd/deploy_project.cs
+++ ISDeploymentCmd/deploy_project.cs
@@ -30,52 +30,5 @@ namespace ISDeploymentCmd
                 }
             }
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="folderName"></param>
-        /// <param name="connection"></param>
-        /// <returns></returns>
-        public int CreateFolder(string folderName, SqlConnection connection = null)
-        {
-            if (string.IsNullOrEmpty(folderName))
-            {
-                Exception ex = new Exception("FolderName must be supplied");
-                throw ex;
-            }
-
-            if (connection == null)
-            {
-                connection = new SqlConnection(ConnectionString);
-            }
-            using (var cmd = new SqlCommand())
-            {
-                cmd.Connection = connection;
-                cmd.CommandText = "catalog.create_folder";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@folder_name", SqlDbType.NVarChar).Value = folderName;
-                cmd.Parameters.Add("@folder_id", SqlDbType.BigInt).Direction = ParameterDirection.Output;
-                connection.Open();
-                try
-                {
-                    int rc = Convert.ToInt32(cmd.ExecuteScalar());
-                    return rc;
-                }
-                catch (SqlException SqlEx)
-                {
-                    // if folder exists error don't error out
-                    if (SqlEx.Number == 27190)
-                    {
-                        return 0;
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-            }
-           
-        }
     }
 };
